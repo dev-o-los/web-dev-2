cc1313927a57e0d967bac09cbda3712a9f778def
devolos <utk27032005@gmail.com>
2024-06-11 19:21:24 +0530

    This commit adds a circle follower element to the HTML code, includes a function to make the circle element follow mouse movements, and incorporates new styling properties for the circle-follower element.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("body"),
   smooth: true,
 });
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    this.document.querySelector(
+      "#circle-follower"
+    ).style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`;
+  });
+}
+
+circleMouseFollower();
